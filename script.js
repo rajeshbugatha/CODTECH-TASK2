2a0f5c982e4f1f926d42c900d0adccb6d3ca6c20
rajeshbugatha <rajeshbugatha7@gmail.com>
2024-11-10 21:21:37 +0530

    Quiz

---
+++ script.js
@@ -0,0 +1,472 @@
+/* Created and coded by Abhilash Narayan */
+/* Quiz source: w3schools.com */
+var quiz = {
+    "JS": [
+    {
+    "id": 1,
+    "question": "HTML Stands for-",
+    "options": [
+    {
+    "a": "HighText Machine Language",
+    "b": "HyperText and links Markup Language",
+    "c": "HyperText Markup Language",
+    "d": "None of the above"
+    }
+    ],
+    "answer": "HyperText Markup Language",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 2,
+    "question": "Which of the following element is responsible for making the text bold in HTML?",
+    "options": [
+    {
+    "a": "&lt;pre&gt;",
+    "b": "&lt;a&gt;",
+    "c": "&lt;b&gt;",
+    "d": "&lt;br&gt;"
+    }
+    ],
+    "answer": "&lt;b&gt;",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 3,
+    "question": "How to create an unordered list (a list with the list items in bullets) in HTML?",
+    "options": [
+    {
+    "a": "&lt;ul&gt;",
+    "b": "&lt;ol&gt;",
+    "c": "&lt;li&gt;",
+    "d": "&lt;l&gt;"
+    }
+    ],
+    "answer": "<ul>",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 4,
+    "question": "The correct sequence of HTML tags for starting a webpage is -",
+    "options": [
+    {
+    "a": "Head, Title, HTML, Body",
+    "b": "HTML, Body, Title, Head",
+    "c": "HTML, Head, Title, Body",
+    "d": "HTML, Title, Head, Body"
+    }
+    ],
+    "answer": "HTML, Head, Title, Body",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 5,
+    "question": "&lt;input&gt; is -",
+    "options": [
+    {
+    "a": "A format tag",
+    "b": "An empty tag",
+    "c": "All of the above",
+    "d": "None of the above",
+    }
+    ],
+    "answer": "An empty tag",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 6,
+    "question": "Which of the following attribute is used to provide a unique name to an element?",
+    "options": [
+    {
+    "a": "class",
+    "b": "id",
+    "c": "type",
+    "d": "none",
+    }
+    ],
+    "answer": "id",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 7,
+    "question": "Which of the following HTML tag is used to display the text with scrolling effect?",
+    "options": [
+    {
+    "a": "&lt;marquee&gt;",
+    "b": "&lt;scroll&gt;",
+    "c": "&lt;div&gt;",
+    "d": "None",
+    }
+    ],
+    "answer": "&lt;marquee&gt;",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 8,
+    "question": "Which of the following HTML attribute is used to define inline styles?",
+    "options": [
+    {
+    "a": "style",
+    "b": "type",
+    "c": "class",
+    "d": "none",
+    }
+    ],
+    "answer": "style",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 9,
+    "question": "An HTML program is saved by using the ____ extension.",
+    "options": [
+    {
+    "a": ".ht",
+    "b": ".html",
+    "c": ".hml",
+    "d": "None of the above"
+    }
+    ],
+    "answer": ".html",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 10,
+    "question": "In HTML5, which of the following tag is used to initialize the document type?",
+    "options": [
+    {
+    "a": "&lt;Doctype HTML&gt;",
+    "b": "&lt;\Doctype HTML&lt;",
+    "c": "&lt;DOCTYPE&gt;",
+    "d": "&lt;!DOCTYPE HTML&gt;",
+    }
+    ],
+    "answer": "&lt;!DOCTYPE HTML&gt;",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 11,
+    "question": "Which of the following tag is used to define options in a drop-down selection list?",
+    "options": [
+    {
+    "a": "&lt;select&gt;",
+    "b": "&lt;list&gt;",
+    "c": "&lt;dropdown&gt;",
+    "d": "&lt;option&gt;"
+    }
+    ],
+    "answer": "&lt;option&gt;",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 12,
+    "question": "The &lt;hr&gt; tag in HTML is used for -",
+    "options": [
+    {
+    "a": "new line",
+    "b": "vertical ruler",
+    "c": "new paragraph",
+    "d": "horizontal ruler"
+    }
+    ],
+    "answer": "horizontal ruler",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 13,
+    "question": "The full form of CSS is:",
+    "options": [
+    {
+    "a": "Cascade style sheets",
+    "b": "Color and style sheets",
+    "c": "Cascading style sheets",
+    "d": "None of the above",
+    }
+    ],
+    "answer": "Cascading style sheets",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 14,
+    "question": "The HTML attribute used to define the inline styles is -",
+    "options": [
+    {
+    "a": "style",
+    "b": "styles",
+    "c": "class",
+    "d": "None",
+    }
+    ],
+    "answer": "style",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 15,
+    "question": "Which of the following is the correct syntax to make the background-color of all paragraph elements to yellow?",
+    "options": [
+    {
+    "a": "p {background-color : yellow;}",
+    "b": "p {background-color : #yellow;}",
+    "c": "all {background-color : yellow;}",
+    "d": "all p {background-color : #yellow;}",
+    }
+    ],
+    "answer": "p {background-color : yellow;}",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 16,
+    "question": "Which of the following is the correct syntax to display the hyperlinks without any underline?",
+    "options": [
+    {
+    "a": "a {text-decoration : underline;}",
+    "b": "a {decoration : no-underline;}",
+    "c": "a {text-decoration : none;}",
+    "d": "None of the above"
+    }
+    ],
+    "answer": "a {text-decoration : none;}",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 17,
+    "question": "The CSS property used to specify the transparency of an element is -",
+    "options": [
+    {
+    "a": "opacity",
+    "b": "filter",
+    "c": "visibility",
+    "d": "overlay"
+    }
+    ],
+    "answer": "opacity",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 18,
+    "question": " Which of the following is not a value of the font-variant property in CSS?",
+    "options": [
+    {
+    "a": "normal",
+    "b": "small-caps",
+    "c": "large-caps",
+    "d": "inherit",
+    }
+    ],
+    "answer": "large-caps",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 19,
+    "question": "JavaScript is the same as Java.",
+    "options": [
+    {
+    "a": "true",
+    "b": "false"
+    }
+    ],
+    "answer": "false",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 20,
+    "question": "How can you detect the client&rsquo;s browser name?",
+    "options": [
+    {
+    "a": "navigator.appName",
+    "b": "browser.name",
+    "c": "client.navName"
+    }
+    ],
+    "answer": "navigator.appName",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 21,
+    "question": "Which event occurs when the user clicks on an HTML element?",
+    "options": [
+    {
+    "a": "onchange",
+    "b": "onclick",
+    "c": "onmouseclick",
+    "d": "onmouseover"
+    }
+    ],
+    "answer": "onclick",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 22,
+    "question": "How do you declare a JavaScript variable?",
+    "options": [
+    {
+    "a": "var carName;",
+    "b": "variable carName;",
+    "c": "v carName;"
+    }
+    ],
+    "answer": "var carName;",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 23,
+    "question": "Which operator is used to assign a value to a variable?",
+    "options": [
+    {
+    "a": "*",
+    "b": "-",
+    "c": "=",
+    "d": "x"
+    }
+    ],
+    "answer": "=",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 24,
+    "question": "What will the following code return: Boolean(10 &gt; 9)",
+    "options": [
+    {
+    "a": "NaN",
+    "b": "false",
+    "c": "true"
+    }
+    ],
+    "answer": "true",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 25,
+    "question": "Is JavaScript case-sensitive?",
+    "options": [
+    {
+    "a": "No",
+    "b": "Yes"
+    }
+    ],
+    "answer": "Yes",
+    "score": 0,
+    "status": ""
+    }
+    ]
+    }
+    var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+    this.currentque = cque;
+    if (this.currentque < totalque) {
+    $("#tque").html(totalque);
+    $("#previous").attr("disabled", false);
+    $("#next").attr("disabled", false);
+    $("#qid").html(quiz.JS[this.currentque].id + '.');
+    $("#question").html(quiz.JS[this.currentque].question);
+    $("#question-options").html("");
+    for (var key in quiz.JS[this.currentque].options[0]) {
+    if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+    $("#question-options").append(
+    "<div class='form-check option-block'>" +
+    "<label class='form-check-label'>" +
+    "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+    quiz.JS[this.currentque].options[0][key] +
+    "</span></label>"
+    );
+    }
+    }
+    }
+    if (this.currentque <= 0) {
+    $("#previous").attr("disabled", true);
+    }
+    if (this.currentque >= totalque) {
+    $('#next').attr('disabled', true);
+    for (var i = 0; i < totalque; i++) {
+    this.score = this.score + quiz.JS[i].score;
+    }
+    return this.showResult(this.score);
+    }
+    }
+    this.showResult = function (scr) {
+    $("#result").addClass('result');
+    $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+    for (var j = 0; j < totalque; j++) {
+    var res;
+    if (quiz.JS[j].score == 0) {
+    res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+    } else {
+    res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+    }
+    $("#result").append(
+    '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+    '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+    '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+    '</div>'
+    );
+    }
+    }
+    this.checkAnswer = function (option) {
+    var answer = quiz.JS[this.currentque].answer;
+    option = option.replace(/</g, "&lt;") //for <
+    option = option.replace(/>/g, "&gt;") //for >
+    option = option.replace(/"/g, "&quot;")
+    if (option == quiz.JS[this.currentque].answer) {
+    if (quiz.JS[this.currentque].score == "") {
+    quiz.JS[this.currentque].score = 1;
+    quiz.JS[this.currentque].status = "correct";
+    }
+    } else {
+    quiz.JS[this.currentque].status = "wrong";
+    }
+    }
+    this.changeQuestion = function (cque) {
+    this.currentque = this.currentque + cque;
+    this.displayQuiz(this.currentque);
+    }
+    }
+    var jsq = new quizApp();
+    var selectedopt;
+    $(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+    //var radio = $(this).find('input:radio');
+    $(this).prop("checked", true);
+    selectedopt = $(this).val();
+    });
+    });
+    $('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+    });
+    $('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+    });
